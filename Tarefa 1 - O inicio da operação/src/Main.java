39a7cd108e3617d5e4790630031028566fb379e8
Icaro Amaral <icarokamaral14@gmail.com>
2026-08-31 18:08:29 -0300

    atualizações na mainn

--- Tarefa 1 - O inicio da operação/src/Main.java
+++ Tarefa 1 - O inicio da operação/src/Main.java
@@ -6,15 +6,21 @@
 *
 */
 
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
 
+        Scanner scanner = new Scanner(System.in);
+
         /*1. Matéria  Prima*/
-        MateriaPrima plasticoABS = new MateriaPrima("ABS000", "Plastico ABS", 100, "kg", 15);
+        MateriaPrima plasticoABS = new MateriaPrima("ABS000", "Plastico ABS", 5000, "g", 200);
 
         System.out.println("Matéria Prima ID: " + plasticoABS.getId() + " - " + plasticoABS.getNome());
         System.out.println("Quantidade de Matéria: " + plasticoABS.getQuantidade() + " " + plasticoABS.getUnidade());
 
-        Produto prod1 = new Produto("CARC_TOP", "Carcaca Superior KTS590", 0)
+        Produto prod1 = new Produto("CARC_TOP", "Carcaca Superior KTS590", 250);
+        Produto prod2 = new Produto("PROT_LAT", "Protecao Lateral KTS590", 200);
+        Produto prod3 = new Produto("CARC_BOT", "Carcaca Inferior KTS590", 250);
     }
 }
