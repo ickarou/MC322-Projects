21edcb980ab3da8e4413fd0cbb6e1b0c88ae0024
Icaro Amaral <icarokamaral14@gmail.com>
2026-09-11 11:28:02 -0300

    feature: cria subclasse CarcacaSuperior

---
+++ Tarefa 1 - O inicio da operação/src/CarcacaSuperior.java
@@ -0,0 +1,33 @@
+/*Subclasse Carcaça Superior
+*
+* Tarefa 2
+*
+* última modificação: 10/09/2026
+*
+* Material para a disciplina MC322 - Programação orientada a objetos
+*
+*/
+
+public class CarcacaSuperior extends Produto{
+
+    public CarcacaSuperior(String id) {
+        super(id, "Carcaça Superior KTS590", 300, 0.9f);
+    }
+
+    /*Métodos da classe principal*/
+    @Override
+    public void processar() {
+        System.out.println("Injetando " + getNome() + "!");
+        aumentarProbabilidadeFalha(0.05f); 
+    }
+
+    @Override
+    public int calcularTempoProducao(){
+        return 150;
+    }
+
+    @Override
+    public String getTipo(){
+        return "Alta Qualidade - Favor, cuidado no manuseio";
+    }
+}
